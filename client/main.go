41593c42d76319580e58765e45ce77ea5350ef1e
guranytou <guranytou@gmail.com>
2023-04-17 10:49:05 +0900

    特定ステータスの時のみリトライ処理をしなくなるのか確認

--- client/main.go
+++ client/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
-	"net/url"
+	"net/http"
 
 	"github.com/hashicorp/go-retryablehttp"
 )
 
 func main() {
-	u := &url.URL{}
-	u.Scheme = "http"
-	u.Host = "localhost:8080"
-	uStr := u.String()
+	retryClient := retryablehttp.NewClient()
+	standardCliend := retryClient.StandardClient()
+	retryClient.CheckRetry = func(ctx context.Context, resp *http.Response, err error) (bool, error) {
+		if resp.StatusCode == http.StatusServiceUnavailable {
+			return false, nil
+		}
+		return retryablehttp.DefaultRetryPolicy(ctx, resp, err)
+	}
 
-	rsp, err := retryablehttp.Get(uStr)
+	req, err := http.NewRequest(
+		"GET",
+		"http://localhost:8080",
+		nil,
+	)
+
+	rsp, err := standardCliend.Do(req)
 	if err != nil {
 		fmt.Println(err)
 		return
--- server/main.go
+++ server/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusBadGateway)
+	w.WriteHeader(http.StatusServiceUnavailable)
 }
 
 func main() {
